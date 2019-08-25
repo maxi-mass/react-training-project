ef0e303302e3e1ee3aee940d219e1ed8e43a0c93
Maxim Svistunov <ma-xsoul@yandex.ru>
2019-08-25 10:07:54 +0400

    34. FLUX-cycle on change every symbol

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,16 +5,21 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
     let postsElements = props.posts.map(p => <Post id={p.id} message={p.message} likeCount={p.likeCount} />);
     let newPostElement = React.createRef();
+
     let addPost = () => {
-       let postText = newPostElement.current.value;
-       props.addPost(postText);
-       newPostElement.current.value = "";
+       props.addPost();
     };
+
+    let onPostChange = () => {
+        let newPostText = newPostElement.current.value;
+        props.changeNewPostText(newPostText);
+    };
+
     return (
         <div className={p.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea ref={newPostElement} value="it-kamasutra"/>
+                <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
             </div>
             <div>
                 <button onClick={addPost}>Add post</button>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,11 @@ const Profile = (props) => {
     return (
         <div>
             <ProfileInfo />
-            <MyPosts posts={props.posts} addPost={props.addPost}/>
+            <MyPosts newPostText={props.profilePage.newPostText}
+                     posts={props.profilePage.posts}
+                     addPost={props.addPost}
+                     changeNewPostText={props.changeNewPostText}
+            />
         </div>
     );
 };
