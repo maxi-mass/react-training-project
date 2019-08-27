497cd18a1b253a5f45f55cbef533d18e0d99244e
Maxim Svistunov <ma-xsoul@yandex.ru>
2019-08-27 09:18:16 +0400

    36. State, store, OOP refactoring. In progress

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,66 @@
 let rerenderEntireTree = () => {};
+export let store  = {
+    rerenderEntireTree(){},
+    _state: {
+        profilePage : {
+            posts : [
+                {id: 1, message: "Hello, how are you?", likeCount: 1},
+                {id: 3, message: "Hi, it's my firs post on this platform!!!", likeCount: 2},
+                {id: 5, message: "some text 11", likeCount: 2},
+                {id: 8, message: "some text 2", likeCount: 3},
+                {id: 11, message: "some text 3", likeCount: 5},
+            ],
+            newPostText : ""
+        },
+        messagesPage : {
+            dialogs :  [
+                {name: "Maximas", id: 11},
+                {name: "Dasha", id: 2},
+                {name: "Ivan", id: 3},
+                {name: "Alex", id: 4},
+                {name: "Statyan", id: 5},
+                {name: "Igoriyan", id: 6}
+            ],
+            messages : [
+                {message: "Hello world", id: 1},
+                {message: "Hey, how are you?", id: 2},
+                {message: "Thanks, I'm fine!", id: 3},
+                {message: "Why nobody loves me?", id: 4},
+                {message: "Stasyan Yo", id: 5},
+                {message: "Igoriyan Yo", id: 6}
+            ],
+            currentMessageId : 7,
+            newMessageText: ""
+        }
+    },
+    addPost() {
+        let newPost = {
+            id : 5,
+            message: state.profilePage.newPostText,
+            likeCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = "";
+        rerenderEntireTree(state);
+    },
+    changeNewPostText(changedNewPostText) {
+        state.profilePage.newPostText = changedNewPostText;
+        rerenderEntireTree(state);
+    },
+    addMessage() {
+        let newMessageItem = {
+            id: this._state.messagesPage.currentMessageId++,
+            message: this._state.messagesPage.newMessageText
+        };
+        this._state.messagesPage.messages.push(newMessageItem);
+        this._state.messagesPage.newMessageText = "";
+        rerenderEntireTree(state);
+    },
+    changeNewMessageText(changedNewMessageText) {
+        this._state.messagesPage.newMessageText = changedNewMessageText;
+        rerenderEntireTree(state);
+    }
+};
 
 let state = {
     profilePage : {
