3106b7218bae5f873996d987bbb4c6a94a88b9a5
Maxim Svistunov <ma-xsoul@yandex.ru>
2019-09-09 14:02:40 +0400

    51. Added axios. Added Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,16 @@
 import React from 'react';
 import usersStyles from './Users.module.css';
+import * as axios from 'axios';
+import defaultAvatar from '../../../src/assets/images/default-avatar.jpg';
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://avatarfiles.alphacoders.com/715/71560.jpg',
-                followed: true,
-                fullName: "Maximas S.",
-                location: {country: "Russia", city: "Nnov"},
-                status: "Hello from NiNo"
-            },
-            {
-                id: 2,
-                photoUrl: 'https://avatarfiles.alphacoders.com/715/71560.jpg',
-                followed: false,
-                fullName: "Dmitry K.",
-                location: {country: "Belarus", city: "Minsk"},
-                status: "Hello from Belarus"
-            },
-            {
-                id: 3,
-                photoUrl: 'https://avatarfiles.alphacoders.com/715/71560.jpg',
-                followed: true,
-                fullName: "Ivan N.",
-                location: {country: "Russia", city: "Moscow"},
-                status: "Hello from Moscow"
-            },
-            {
-                id: 4,
-                photoUrl: 'https://avatarfiles.alphacoders.com/715/71560.jpg',
-                followed: true,
-                fullName: "Vladimir P.",
-                location: {country: "Russia", city: "St.Petersburg"},
-                status: "Hello from Piter"
-            },
-            {
-                id: 5,
-                photoUrl: 'https://avatarfiles.alphacoders.com/715/71560.jpg',
-                followed: false,
-                fullName: "Alexey I.",
-                location: {country: "Russia", city: "Saratov"},
-                status: "Hellow from Saratov"
-            },
-        ]);
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users?page=60")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
+
     }
     return <div>
         {
@@ -52,7 +18,7 @@ const Users = (props) => {
                 return <div key={user.id} className={usersStyles.userItem}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} alt="avatar"/>
+                            <img src={user.photos.small ? user.photos.small : defaultAvatar} alt="avatar"/>
                         </div>
                         <div>
                             {
@@ -65,12 +31,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
                         <span>
-                            <div>{user.location.country}</div>
-                            <div>{user.location.city}</div>
+                            <div>{"user.location.country"}</div>
+                            <div>{"user.location.city"}</div>
                         </span>
                     </span>
                 </div>
