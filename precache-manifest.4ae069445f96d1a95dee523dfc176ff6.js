32fae70103e87df684783cda934886fa24fd7345
Maxim Svistunov <svistunovms@lad24.ru>
2019-12-04 09:15:55 +0300

    Updates

---
+++ precache-manifest.4ae069445f96d1a95dee523dfc176ff6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7c96468435f1fc15477477e5305cf7",
+    "url": "/react-training-project/index.html"
+  },
+  {
+    "revision": "ec0f5d76896438f1bc3e",
+    "url": "/react-training-project/static/css/3.0cfdbf34.chunk.css"
+  },
+  {
+    "revision": "c18e0af2a70c2cb2c3c8",
+    "url": "/react-training-project/static/css/4.e58f4236.chunk.css"
+  },
+  {
+    "revision": "95aecb7e32cfdd8c1bfb",
+    "url": "/react-training-project/static/css/main.e44737b4.chunk.css"
+  },
+  {
+    "revision": "0975a5d01b24dc70a6ca",
+    "url": "/react-training-project/static/js/2.6b177c0a.chunk.js"
+  },
+  {
+    "revision": "ec0f5d76896438f1bc3e",
+    "url": "/react-training-project/static/js/3.07fe5676.chunk.js"
+  },
+  {
+    "revision": "c18e0af2a70c2cb2c3c8",
+    "url": "/react-training-project/static/js/4.eb5f7c90.chunk.js"
+  },
+  {
+    "revision": "95aecb7e32cfdd8c1bfb",
+    "url": "/react-training-project/static/js/main.e655c6ff.chunk.js"
+  },
+  {
+    "revision": "c65e832ec5195864640e",
+    "url": "/react-training-project/static/js/runtime-main.0b2d3b2c.js"
+  },
+  {
+    "revision": "2f2ad9512c7ad4ea794d3a5d6adbd69e",
+    "url": "/react-training-project/static/media/spinner.2f2ad951.gif"
+  }
+]);
